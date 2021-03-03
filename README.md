540c91aff5887043882de1d761df75122ba3a92a
Ezequiel Zacca <ezequielzacca@gmail.com>
2021-03-03 10:42:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Merlin GQL - Core Library
+
+This repository is part of the **Merlin GQL Framework**
+
+You can learn more about it in [Merlin GQL - CLI](https://github.com/silentium-labs/merlin-gql-cli)
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+type Action = { type: string }
+let executedActions: Action[] = [];
+let initialState: any;
+const UNDO_ACTION_TYPE = 'UNDO'
+
+export const undoable = (rootReducer: any, bufferSize: number = 100) => {
+    return (state: any, action: any) => {
+        if (action.type === UNDO_ACTION_TYPE) {
+            let newState: any = initialState;
+            executedActions = executedActions.filter(eAct => eAct !== action.payload);
+            executedActions.forEach(executedAction =>
+                newState = rootReducer(newState, executedAction));
+            return newState;
+        }
+        executedActions.push(action);
+        const updatedState = rootReducer(state, action);
+        //Handle buffer overflow
+        if (executedActions.length === bufferSize + 1) {
+            const firstAction = executedActions[0];
+            initialState = rootReducer(initialState, firstAction);
+            executedActions = executedActions.slice(1, bufferSize + 1);
+        }
+        return updatedState;
+    }
+}
+
+export const undo = (action: Action) => ({ type: UNDO_ACTION_TYPE, payload: action })
